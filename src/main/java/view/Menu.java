8f7999cfefe577bfcbfeb475be4055799b380677
Carlos <carlosmrd2001@gmail.com>
2025-06-02 20:05:07 -0300

    Atualização do Menu

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -6,16 +6,46 @@ public class Menu {
     static Scanner scanner = new Scanner(System.in);
 
     public int exibirMenuPrincipal() {
-            System.out.println("\n----- MENU -----");
-            System.out.println("1. Cadastrar cliente");
-            System.out.println("3. Buscar cliente");
-            System.out.println("4. Listar clientes");
-            System.out.println("5. Editar cliente");
-            System.out.println("6. Adicionar lembrete");
-            System.out.println("7. Adicionar anotação");
-            System.out.println("0. Sair");
-            System.out.print("Escolha uma opção: ");
+        System.out.println("\n----- MENU -----");
+        System.out.println("1. Clientes");
+        System.out.println("2. Anotações");
+        System.out.println("3. Lembretes");
+        System.out.println("0. Sair");
+        System.out.print("Escolha uma opção: ");
 
-            return scanner.nextInt();
+        return scanner.nextInt();
     }
+
+    public int exibirMenuClientes() {
+        System.out.println("1. Cadastrar cliente");
+        System.out.println("2. Listar clientes");
+        System.out.println("3. Buscar cliente");
+        System.out.println("4. Alterar cliente");
+        System.out.println("5. Excluir cliente");
+        System.out.println("0. Voltar");
+
+        return scanner.nextInt();
+    }
+
+    public int exibirMenuAnotacoes() {
+        System.out.println("1. Criar anotação");
+        System.out.println("2. Buscar anotações");
+        System.out.println("3. Alterar anotação");
+        System.out.println("4. Excluir anotação");
+        System.out.println("0. Voltar");
+
+        return scanner.nextInt();
+    }
+
+    public int exibirMenuLembretes() {
+        System.out.println("1. Criar lembrete");
+        System.out.println("1. Listar lembretes");
+        System.out.println("2. Buscar lembretes");
+        System.out.println("3. Alterar lembrete");
+        System.out.println("4. Excluir lembrete");
+        System.out.println("0. Voltar");
+
+        return scanner.nextInt();
+    }
+
 }
