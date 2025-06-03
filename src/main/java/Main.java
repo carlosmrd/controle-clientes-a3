0825ca6a614097947c02bca8631186bc195d0bd1
Carlos <carlosmrd2001@gmail.com>
2025-06-02 23:10:41 -0300

    Atualização do Menu, criação dos Controllers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,47 @@ import database.InicializadorDB;
 import view.Menu;
 
 public class Main {
+
     public static void main(String[] args) {
 
         InicializadorDB.inicializar();
-        Menu menu = new Menu();
 
-        int escolha;
+        Menu menu = new Menu();
+        int opcao;
 
         do {
-            escolha = menu.exibirMenuPrincipal();
+            opcao = menu.exibirMenuPrincipal();
 
-            switch (escolha) {
+            switch (opcao) {
+                case 1:
+                    menuClientes(menu);
+                    break;
+
+                case 2:
+                    menuAnotacoes(menu);
+                    break;
+
+                case 3:
+                    menuLembretes(menu);
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o sistema.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida, tente novamente.");
+            }
+
+        } while (opcao != 0);
+    }
+
+    private static void menuClientes(Menu menu) {
+        int opcao;
+        do {
+            opcao = menu.exibirMenuClientes();
+
+            switch (opcao) {
                 case 1:
 
                     break;
@@ -21,14 +51,97 @@ public class Main {
 
                     break;
 
-                case 0:
-                    System.out.println("Encerrando");
+                case 3:
+
                     break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 0:
+                    return;
 
                 default:
                     System.out.println("Opção inválida, tente novamente.");
             }
-        } while (escolha != 0);
 
+        } while (opcao != 0);
+    }
+
+    private static void menuAnotacoes(Menu menu) {
+        int opcao;
+        do {
+            opcao = menu.exibirMenuAnotacoes();
+
+            switch (opcao) {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 0:
+                    return;
+
+                default:
+                    System.out.println("Opção inválida, tente novamente.");
+            }
+
+        } while (opcao != 0);
+    }
+
+    private static void menuLembretes(Menu menu) {
+        int opcao;
+        do {
+            opcao = menu.exibirMenuLembretes();
+
+            switch (opcao) {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 0:
+                    return;
+
+                default:
+                    System.out.println("Opção inválida, tente novamente.");
+            }
+
+        } while (opcao != 0);
     }
 }
--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -17,35 +17,40 @@ public class Menu {
     }
 
     public int exibirMenuClientes() {
+        System.out.println("\n--- CLIENTES ---");
         System.out.println("1. Cadastrar cliente");
         System.out.println("2. Listar clientes");
         System.out.println("3. Buscar cliente");
         System.out.println("4. Alterar cliente");
         System.out.println("5. Excluir cliente");
         System.out.println("0. Voltar");
+        System.out.print("Escolha uma opção: ");
 
         return scanner.nextInt();
     }
 
     public int exibirMenuAnotacoes() {
+        System.out.println("\n--- ANOTAÇÕES ---");
         System.out.println("1. Criar anotação");
         System.out.println("2. Buscar anotações");
         System.out.println("3. Alterar anotação");
         System.out.println("4. Excluir anotação");
         System.out.println("0. Voltar");
+        System.out.print("Escolha uma opção: ");
 
         return scanner.nextInt();
     }
 
     public int exibirMenuLembretes() {
+        System.out.println("\n--- LEMBRETES ---");
         System.out.println("1. Criar lembrete");
-        System.out.println("1. Listar lembretes");
-        System.out.println("2. Buscar lembretes");
-        System.out.println("3. Alterar lembrete");
-        System.out.println("4. Excluir lembrete");
+        System.out.println("2. Listar lembretes");
+        System.out.println("3. Buscar lembretes");
+        System.out.println("4. Alterar lembrete");
+        System.out.println("5. Excluir lembrete");
         System.out.println("0. Voltar");
+        System.out.print("Escolha uma opção: ");
 
         return scanner.nextInt();
     }
-
 }
