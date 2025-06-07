1a6830650fd444f2b603f35fedcdfcbfe17aee90
Carlos <carlosmrd2001@gmail.com>
2025-06-07 17:53:03 -0300

    Construtores e atributos de Anotacao.java

--- src/main/java/model/Anotacao.java
+++ src/main/java/model/Anotacao.java
@@ -1,4 +1,59 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Anotacao {
+
+    private int id;
+    private int idCliente;
+    private String descricaoAnotacao;
+    private LocalDateTime dataHora;
+
+    //Construtor de Anotação sem ID, usado no registro
+    public Anotacao (int idCliente, String descricaoAnotacao, LocalDateTime dataHora) {
+        this.idCliente = idCliente;
+        this.descricaoAnotacao = descricaoAnotacao;
+        this.dataHora = dataHora;
+    }
+
+    //Construtor de Anotação com todos os dados
+    public Anotacao (int id, int idCliente, String descricaoAnotacao, LocalDateTime dataHora) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.descricaoAnotacao = descricaoAnotacao;
+        this.dataHora = dataHora;
+    }
+
+    //Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int id) {
+        this.idCliente = idCliente;
+    }
+
+    public String getDescricaoAnotacao() {
+        return descricaoAnotacao;
+    }
+
+    public void setId(String descricaoAnotacao) {
+        this.descricaoAnotacao = descricaoAnotacao;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
 }
